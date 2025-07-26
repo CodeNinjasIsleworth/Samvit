e2bbaf1c840b84835bd74953214533103c79b69b
Codeninja1-tech <yilege7643@ronvoto.com>
2025-07-26 11:18:43 +0100

    Add Poly Run V1 Project
    Add Poly Run V1 Project

---
+++ Samvit Poly Run New/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Default Jumping Power")]
+    public float jumpPower = 6f;
+    [Header("Boolean isGrounded")]
+    public bool isGrounded;
+    float posX = 0.0f;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody2D>();
+        posX = transform.position.x;
+        Time.timeScale = 1;
+    }
+
+    void GameOver()
+    {
+        GameObject.Find("GameController").GetComponent<GameController>().GameOver();
+        //Time.timeScale = 0;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpPower * rb.mass * rb.gravityScale * 20.0f);
+        }
+        if (transform.position.x < posX)
+        {
+            GameOver();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+        if (collision.gameObject.tag == "Enemy")
+        {
+            GameOver();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Coin")
+        {
+            GameObject.Find("GameController").GetComponent<GameController>().IncrementScore();
+            Destroy(collision.gameObject);
+        }
+    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
---
+++ Samvit Poly Run New/Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("ChallengeObj Game Object")]
+    public GameObject challengeObject;
+    [Header("Default delay spawn time")]
+    public float spawnDelay = 1f;
+    [Header("Default spawn time")]
+    public float spawnTime = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("InstantiateObjects", spawnDelay, spawnTime);
+        }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(5, 0f, 0);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            spawnTime = 0.1f;
+        }
+            
+    }
+
+        void InstantiateObjects()
+    {
+    Instantiate(challengeObject, transform.position, transform.rotation);
+    }
+}
+
+    
